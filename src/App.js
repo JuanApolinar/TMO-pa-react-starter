275f293cb07bbb3838b712c04a07b2632484bef5
Juan Apolinar <juanapolinar@my.unt.edu>
2021-09-08 14:41:07 -0500

    moved away from component into main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import './App.css';
-import Recipes from './components/Recipes';
+import { useState } from "react";
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [list2, setList2] = useState([])
+  const [name, setName] = useState('')
+  const [instruct, setInstruct] = useState('')
+
+  const [showing, setShowing] = useState(false);
+  
+  function handleChange(event) {
+      setName(event.target.value);
+    }
+
+  function handleChange2(event) {
+      setInstruct(event.target.value);
+  }
+
+  function handleAdd() {
+      const newList = list.concat({ name });
+      const newList2 = list2.concat({ instruct })
+      setList(newList);
+      setList2(newList2);
+      setName('')
+      setInstruct('')
+    }
+
   return (
     <div>
       <h1 className="doNotRemoveMe">Hello world.</h1>
       {/* ^ Do not remove this element ^ */}
       <h1>My Recipes</h1>
-      <Recipes />
+      <div>
+        {list.length > 0 ? (        
+        <ul>
+            {list.map((item) => (
+                <li key={item.id}>{item.name}</li>
+            ))}
+            {/* {list2.map((item) => (
+                <li key={item.id}>{item.instruct}</li>
+            ))} */}
+        </ul>) :
+        <div>
+            There are no recipes to list    
+        </div>}
+
+           {!showing ? <button onClick={() => setShowing({ showing: !showing })}>Add Recipe</button> : null}
+                { showing ?  <form>
+                                <label>recipe-name
+                                <input type="text" value={name} name="recipe-name" onChange={handleChange} />
+                                </label>
+                                <label>recipe-instructions
+                                <input type="text" value={instruct} name="recipe-instructions" onChange={handleChange2} />
+                                </label>
+                                    <button type="button" onClick={handleAdd}>
+                                        Submit
+                                    </button>
+                            </form> 
+                : null
+                }
+        
+
+    </div>
     </div>
   );
 }
